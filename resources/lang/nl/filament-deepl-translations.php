e4a672301d3c7f0b086f38178fc07a3bc0eb3ca7
Martijn Wagena <martijn@concept7.nl>
2024-07-24 16:31:15 +0200

    updated translations

---
+++ resources/lang/nl/filament-deepl-translations.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'translate' => 'Vertalen',
+    'modal_title' => 'Vertalen met DeepL',
+    'source' => 'Originele taal',
+    'success_title' => 'Success!',
+    'success_message' => 'Content is vertaald.',
+    'error_title' => 'Error!',
+    'error_message' => 'Content kon niet vertaald worden.',
+    'multiple' => [
+        'label' => 'Vertalen met DeepL',
+        'modal' => [
+            'heading' => 'Overschrijf alle vertalingen',
+        ],
+    ],
+];
